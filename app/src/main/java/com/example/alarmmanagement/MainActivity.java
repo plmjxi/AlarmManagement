0b86b29c93862dc83a5a76091e6e7171cc04ecf8
pluemjai <pluemzajai12345@gmail.com>
2023-10-15 21:28:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/alarmmanagement/MainActivity.java
@@ -0,0 +1,44 @@
+    package com.example.alarmmanagement;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.alarmmanagement.databinding.ActivityMainBinding;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                finish();
+            }
+        },1000);
+
+
+    }
+
+}
